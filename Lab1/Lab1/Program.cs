d3036ac520e28f15a32664c0916c3086f189fd16
rsukhaniuk <rsukhaniuk@gmail.com>
2024-10-23 23:02:22 +0300

    add prometheus, graphana

--- Lab1/Lab1/Program.cs
+++ Lab1/Lab1/Program.cs
@@ -1,3 +1,5 @@
+using Prometheus;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.UseHttpClientMetrics();
+
 
 var app = builder.Build();
 
@@ -21,5 +25,8 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+app.UseMetricServer();
+app.UseHttpMetrics();
 
 app.Run();
