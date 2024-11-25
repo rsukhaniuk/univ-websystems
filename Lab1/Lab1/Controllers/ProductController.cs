0ca7835621a02f8c35463a86a3c749d3af0d3639
rsukhaniuk <rsukhaniuk@gmail.com>
2024-11-25 18:35:07 +0200

    add caching to GetProduct method with MemoryCache

--- Lab1/Lab1/Controllers/ProductController.cs
+++ Lab1/Lab1/Controllers/ProductController.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Mvc;
 using Lab1.Models;
 using Microsoft.EntityFrameworkCore;
 using univ_websystems_test.Data;
+using Microsoft.Extensions.Caching.Memory;
 
 namespace Lab1.Controllers
 {
@@ -9,6 +10,12 @@ namespace Lab1.Controllers
     [ApiController]
     public class ProductController : ControllerBase
     {
+        private readonly IMemoryCache _cache;
+
+        public ProductController(IMemoryCache cache)
+        {
+            _cache = cache;
+        }
 
         [HttpGet("{id:int}")]
         [ProducesResponseType(StatusCodes.Status200OK)]
@@ -16,14 +23,26 @@ namespace Lab1.Controllers
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         public async Task<ActionResult<Product>> GetProduct(int id)
         {
-
             if (id <= 0)
                 return BadRequest();
 
-            var product = ProductStore.Products.FirstOrDefault(a => a.Id == id);
+            var cacheKey = $"Product_{id}";
 
-            if (product == null)
-                return NotFound();
+            if (!_cache.TryGetValue(cacheKey, out Product? product))
+            {
+                product = ProductStore.Products.FirstOrDefault(a => a.Id == id);
+
+                if (product == null)
+                    return NotFound();
+
+                var cacheEntryOptions = new MemoryCacheEntryOptions()
+                    .SetAbsoluteExpiration(TimeSpan.FromMinutes(5))  // TTL
+                    .SetSlidingExpiration(TimeSpan.FromMinutes(2)); // TTI
+
+                _cache.Set(cacheKey, product, cacheEntryOptions);
+
+                await Task.Delay(2000);
+            }
 
             return Ok(product);
         }
